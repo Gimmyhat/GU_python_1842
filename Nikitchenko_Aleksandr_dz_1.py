d27d17673c954671a11986c729b9d23962098c75
Gimmyhat <nikitchenko.as@gmail.com>
2021-10-14 14:40:07 +0800

    Задание 1. удалил ошибку в 13 строке

--- Nikitchenko_Aleksandr_dz_1.py
+++ Nikitchenko_Aleksandr_dz_1.py
@@ -10,7 +10,8 @@ while True:
         time_digit = seconds_convertor(int(input('Введите количество секунд (q - выход): ')))
         time_text = ['дн', 'час', 'мин', 'сек']
         while time_digit[0] == 0: # приводим вид "0 дн 0 мес 1 мин 1 сек" к виду "1 мин 1 сек"
-            time_digit.pop(0) and time_text.pop(0)
+            time_digit.pop(0)
+            time_text.pop(0)
         for i in range(len(time_digit)):
             print(time_digit[i], time_text[i], end=' ')
         print()
