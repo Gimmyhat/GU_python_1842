b1d6b7f26d3906b7010b8d48e0c38f3bf73a3a23
Gimmyhat <nikitchenko.as@gmail.com>
2021-10-31 08:51:23 +0800

    Урок 6. Задача 5 (чуть причесал код)

--- Nikitchenko_A_lesson_6_dz_5.py
+++ Nikitchenko_A_lesson_6_dz_5.py
@@ -16,7 +16,7 @@ def generate_hobby(n=5):  # Генерируем список хобби
     return [(', '.join(random.sample(hobby, random.randint(1, 3)))) + '\n' for _ in range(n)]
 
 
-def read_write(file, user_list=None, oper='r'):
+def read_write(file, user_list=None, oper='r'):  # Функция записи/чтения файла
     with open(file, oper, encoding='utf-8') as f:
         if oper == 'w':
             f.writelines(user_list)
@@ -26,10 +26,8 @@ def read_write(file, user_list=None, oper='r'):
 
 if __name__ == '__main__':
     n_users = 7
-    n_hobby = 6
-
+    n_hobby = 5
     try:
-
         if sys.argv[1:]:
             file_users = sys.argv[1]
             file_hobby = sys.argv[2]
@@ -38,30 +36,23 @@ if __name__ == '__main__':
             file_users = 'users.csv'
             file_hobby = 'hobby.csv'
             file_result = 'users_hobby.txt'
-
-    except IndexError:
-
-        print(f'Аргументов должно быть 3')
-
-    else:
-
         # Запись в файл
         read_write(file_users, generate_users(n_users), 'w')
         read_write(file_hobby, generate_hobby(n_hobby), 'w')
-
         # Чтение из файла в список
         l_users = read_write(file_users)
         l_hobby = read_write(file_hobby)
-
         diff = len(l_users) - len(l_hobby)
+        # Если в файле, хранящем данные о хобби, меньше записей, чем в файле с ФИО, задаём в словаре значение None
         if diff >= 0:
             users_hobby = []
             l_hobby.extend([None] * diff)
             for x, y in zip(l_users, l_hobby):
                 users_hobby.append(f'{x}: {y}\n')
-            with open(file_result, 'w', encoding='utf-8') as f:
-                f.writelines(users_hobby)
+            read_write(file_result, users_hobby, 'w')
+        # Если наоборот — выходим из скрипта с кодом «1»
         elif diff < 0:
             print('1')
-
-        print('Задача выполнена!')
+    except IndexError:
+        print(f'Аргументов должно быть 3')
+    print('Задача выполнена!')
